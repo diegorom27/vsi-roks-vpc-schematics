35a211d84a17e2bcdbc98b8827bfaadeaeabd06d
diegorom27 <diegorom27@gmail.com>
2024-07-02 16:34:15 -0500

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,7 +1,3 @@
-#variable ibmcloud_api_key {
-#    description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
-#    type        = string
-#}
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     type        = string
