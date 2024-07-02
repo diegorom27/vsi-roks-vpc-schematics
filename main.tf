30117a59f15a8b2bca12da051f0758b08172993d
diegorom27 <diegorom27@gmail.com>
2024-07-02 18:58:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,15 +59,6 @@ data "ibm_is_subnet" "pr_subnet" {
 # OS_RHEL_8_X_64_BIT_PER_PROCESSOR_LICENSING      REDHAT_8_64
 ##############################################################################
 
-
-#resource "ibm_is_volume" "control_plane_storage" {
-#    count =  3
-#    name = "controlplane0${count.index + 1}.storage.satellite-demo.cloud"
-#    zone       = "dal13"
-#    profile = "5iops-tier"
-#    capacity = 100
-#}
-
 resource "ibm_is_instance" "control_plane" {
     for_each = { for vm in var.control_plane : vm.hostname => vm }
     provider = ibm.primary
@@ -133,9 +124,129 @@ resource "ibm_is_instance_volume_attachment" "control_plane_storage" {
 # Worker nodes
 ##############################################################################
 
+resource "ibm_is_instance" "worker_nodes" {
+    for_each = { for vm in var.worker_nodes : vm.hostname => vm }
+    provider = ibm.primary
+    name    = each.value.hostname
+    profile = "bx2d-4x16"
+    image = var.rhel_image
 
+    primary_network_interface {
+      subnet = data.ibm_is_subnet.pr_subnet.id
+    }
+
+    vpc       = data.ibm_is_vpc.pr_vpc.id
+    zone      = "${var.ibm_region}-${var.subnet_zone}"
+    keys      = [data.ibm_is_ssh_key.sshkey.id]
+    resource_group = data.ibm_resource_group.group.id
+
+    boot_volume{
+        name = "boot-volume-${each.value.hostname}"
+        size = 100
+    }
+}
+
+locals {
+  instance_ids_worker_nodes = { for k, v in ibm_is_instance.worker_nodes : k => v.id }
+}
+
+locals {
+  # Flatten to a list of maps where each map contains `hostname`, `disk_index`, and `disk_size`
+  flattened_disks_worker_nodes = flatten([
+    for cp in var.worker_nodes : [
+      for idx, size in cp.disks : {
+        hostname = cp.hostname
+        disk_index = idx
+        disk_size = size
+      }
+    ]
+  ])
+
+  # Create a map with keys in the format of `hostname-disk_index`
+  volume_attachment_map_worker_nodes = {
+    for disk in local.flattened_disks_worker_nodes : "${disk.hostname}-${disk.disk_index}" => disk
+  }
+}
+
+resource "ibm_is_instance_volume_attachment" "control_plane_storage" {
+  for_each = local.volume_attachment_map_worker_nodes
+
+  instance                           = local.instance_ids_worker_nodes[each.value.hostname]
+  name                               = "storage-attachment-${each.key}"
+  capacity                           = each.value.disk_size
+  delete_volume_on_attachment_delete = true
+  delete_volume_on_instance_delete   = true
+  volume_name                        = "storage-${each.key}"
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
 
 ##############################################################################
 # ODF 
 # profile bx2-8x32
 ##############################################################################
+
+resource "ibm_is_instance" "odf" {
+    for_each = { for vm in var.ODF : vm.hostname => vm }
+    provider = ibm.primary
+    name    = each.value.hostname
+    profile = "bx2-8x32"
+    image = var.rhel_image
+
+    primary_network_interface {
+      subnet = data.ibm_is_subnet.pr_subnet.id
+    }
+
+    vpc       = data.ibm_is_vpc.pr_vpc.id
+    zone      = "${var.ibm_region}-${var.subnet_zone}"
+    keys      = [data.ibm_is_ssh_key.sshkey.id]
+    resource_group = data.ibm_resource_group.group.id
+
+    boot_volume{
+        name = "boot-volume-${each.value.hostname}"
+        size = 100
+    }
+}
+
+locals {
+  instance_ids_odf = { for k, v in ibm_is_instance.odf : k => v.id }
+}
+
+locals {
+  # Flatten to a list of maps where each map contains `hostname`, `disk_index`, and `disk_size`
+  flattened_disks_odf= flatten([
+    for cp in var.ODF : [
+      for idx, size in cp.disks : {
+        hostname = cp.hostname
+        disk_index = idx
+        disk_size = size
+      }
+    ]
+  ])
+
+  # Create a map with keys in the format of `hostname-disk_index`
+  volume_attachment_map_odf = {
+    for disk in local.flattened_disks_odf : "${disk.hostname}-${disk.disk_index}" => disk
+  }
+}
+
+resource "ibm_is_instance_volume_attachment" "control_plane_storage" {
+  for_each = local.volume_attachment_map_odf
+
+  instance                           = local.instance_ids_odf[each.value.hostname]
+  name                               = "storage-attachment-${each.key}"
+  capacity                           = each.value.disk_size
+  delete_volume_on_attachment_delete = true
+  delete_volume_on_instance_delete   = true
+  volume_name                        = "storage-${each.key}"
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
